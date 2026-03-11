deb490eb7ae0e2153e6f70d6f2addd69900333de
Vo Phi Hoang <kilogam145236@gmail.com>
2026-03-11 22:15:59 +0700

    [fix] knight and king ui piece

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,64 +1,442 @@
 const CELL_SIZE = 60;
+const PIECE_MAP = {
+  K: "♔",
+  Q: "♕",
+  R: "♖",
+  B: "♗",
+  N: "♘",
+  P: "♙",
+  k: "♚",
+  q: "♛",
+  r: "♜",
+  b: "♝",
+  n: "♞",
+  p: "♟",
+};
+const PIECE = {
+  // quân trắng
+  "♔": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      >
+        <path strokeLinejoin="miter" d="M22.5 11.63V6M20 8h5" />
+
+        <path
+          fill="#fff"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          d="M22.5 25s4.5-7.5 3-10.5c0 0-1-2.5-3-2.5s-3 2.5-3 2.5c-1.5 3 3 10.5 3 10.5"
+        />
+
+        <path
+          fill="#fff"
+          d="M12.5 37c5.5 3.5 14.5 3.5 20 0v-7s9-4.5 6-10.5c-4-6.5-13.5-3.5-16 4V27v-3.5c-2.5-7.5-12-10.5-16-4-3 6 6 10.5 6 10.5v7"
+        />
+
+        <path d="M12.5 30c5.5-3 14.5-3 20 0m-20 3.5c5.5-3 14.5-3 20 0m-20 3.5c5.5-3 14.5-3 20 0" />
+      </g>
+    </svg>
+  ),
+  "♕": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fill="#fff"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      >
+        <path
+          d="M9 26c8.5-1.5 21-1.5 27 0l2-12-7 11V11l-5.5 13.5-3-15-3 15L14 11v14L7 14l2 12z"
+          strokeLinecap="butt"
+        />
+        <path
+          d="M9 26c0 2 1.5 2 2.5 4 1 1.5 1 1 .5 3.5-1.5 1-1.5 2.5-1.5 2.5-1.5 1.5.5 2.5.5 2.5 6.5 1 16.5 1 23 0 0 0 1.5-1 0-2.5 0 0 .5-1.5-1-2.5-.5-2.5-.5-2 .5-3.5 1-2 2.5-2 2.5-4-8.5-1.5-18.5-1.5-27 0z"
+          strokeLinecap="butt"
+        />
+        <path
+          d="M11.5 30c3.5-1 18.5-1 22 0M12 33.5c4-1.5 17-1.5 21 0"
+          fill="none"
+        />
+        <circle cx="6" cy="12" r="2" />
+        <circle cx="14" cy="9" r="2" />
+        <circle cx="22.5" cy="8" r="2" />
+        <circle cx="31" cy="9" r="2" />
+        <circle cx="39" cy="12" r="2" />
+      </g>
+    </svg>
+  ),
+  "♖": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fillRule="evenodd"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path
+          d="M9 39h27v-3H9v3zM12 36v-4h21v4H12zM11 14V9h4v2h5V9h5v2h5V9h4v5"
+          fill="#fff"
+          strokeLinecap="butt"
+        />
+        <path
+          d="M34 14l-3 3H14l-3-3"
+          fill="#fff"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+        />
+        <path
+          d="M31 17v12.5H14V17"
+          fill="#fff"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+        />
+        <path
+          d="M31 29.5l1.5 2.5h-20l1.5-2.5"
+          fill="#fff"
+          strokeLinecap="butt"
+        />
+        <path d="M11 14h23" fill="none" strokeLinejoin="miter" />
+      </g>
+    </svg>
+  ),
+  "♗": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <g fill="#fff" strokeLinecap="butt">
+          <path d="M9 36c3.39-.97 10.11.43 13.5-2 3.39 2.43 10.11 1.03 13.5 2 0 0 1.65.54 3 2-.68.97-1.65.99-3 .5-3.39-.97-10.11.46-13.5-1-3.39 1.46-10.11.03-13.5 1-1.354.49-2.323.47-3-.5 1.354-1.94 3-2 3-2z" />
+          <path d="M15 32c2.5 2.5 12.5 2.5 15 0 .5-1.5 0-2 0-2 0-2.5-2.5-4-2.5-4 5.5-1.5 6-11.5-5-15.5-11 4-10.5 14-5 15.5 0 0-2.5 1.5-2.5 4 0 0-.5.5 0 2z" />
+          <path d="M25 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+        </g>
+        <path
+          d="M17.5 26h10M15 30h15m-7.5-14.5v5M20 18h5"
+          strokeLinejoin="miter"
+        />
+      </g>
+    </svg>
+  ),
+  "♘": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        opacity="1"
+        fill="none"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="#000000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="4"
+        strokeDasharray="none"
+        strokeOpacity="1"
+        transform="translate(0,0.3)"
+      >
+        <path
+          d="M22 10C32.5 11 38.5 18 38 39H15C15 30 25 32.5 23 18"
+          fill="#ffffff"
+          stroke="#000000"
+        />
+
+        <path
+          d="M24 18C24.38 20.91 18.45 25.37 16 27C13 29 13.18 31.34 11 31C9.958 30.06 12.41 27.96 11 28C10 28 11.19 29.23 10 30C9 30 5.997 31 6 26C6 24 12 14 12 14C12 14 13.89 12.1 14 10.5C13.27 9.506 13.5 8.5 13.5 7.5C14.5 6.5 16.5 10 16.5 10H18.5C18.5 10 19.28 8.008 21 7C22 7 22 10 22 10"
+          fill="#ffffff"
+          stroke="#000000"
+        />
+
+        <path
+          d="M9.5 25.5A0.5 0.5 0 1 1 8.5 25.5A0.5 0.5 0 1 1 9.5 25.5z"
+          fill="#000000"
+          stroke="#000000"
+        />
+
+        <path
+          d="M15 15.5A0.5 1.5 0 1 1 14 15.5A0.5 1.5 0 1 1 15 15.5z"
+          transform="matrix(0.866,0.5,-0.5,0.866,9.693,-5.173)"
+          fill="#000000"
+          stroke="#000000"
+        />
+      </g>
+    </svg>
+  ),
+  "♙": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <path
+        d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38-1.95 1.12-3.28 3.21-3.28 5.62 0 2.03.94 3.84 2.41 5.03-3 1.06-7.41 5.55-7.41 13.47h23c0-7.92-4.41-12.41-7.41-13.47 1.47-1.19 2.41-3 2.41-5.03 0-2.41-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4z"
+        fill="#fff"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  ),
+  //quân đen
+  "♚": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+      >
+        <path strokeLinejoin="miter" d="M22.5 11.63V6M20 8h5" />
+
+        <path
+          fill="#000"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          d="M22.5 25s4.5-7.5 3-10.5c0 0-1-2.5-3-2.5s-3 2.5-3 2.5c-1.5 3 3 10.5 3 10.5"
+        />
+
+        <path
+          fill="#000"
+          d="M12.5 37c5.5 3.5 14.5 3.5 20 0v-7s9-4.5 6-10.5c-4-6.5-13.5-3.5-16 4V27v-3.5c-2.5-7.5-12-10.5-16-4-3 6 6 10.5 6 10.5v7"
+        />
+
+        <path d="M12.5 30c5.5-3 14.5-3 20 0m-20 3.5c5.5-3 14.5-3 20 0m-20 3.5c5.5-3 14.5-3 20 0" />
+      </g>
+    </svg>
+  ),
+  "♛": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fill="#000"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      >
+        <path
+          d="M9 26c8.5-1.5 21-1.5 27 0l2-12-7 11V11l-5.5 13.5-3-15-3 15L14 11v14L7 14l2 12z"
+          strokeLinecap="butt"
+        />
+        <path
+          d="M9 26c0 2 1.5 2 2.5 4 1 1.5 1 1 .5 3.5-1.5 1-1.5 2.5-1.5 2.5-1.5 1.5.5 2.5.5 2.5 6.5 1 16.5 1 23 0 0 0 1.5-1 0-2.5 0 0 .5-1.5-1-2.5-.5-2.5-.5-2 .5-3.5 1-2 2.5-2 2.5-4-8.5-1.5-18.5-1.5-27 0z"
+          strokeLinecap="butt"
+        />
+        <path
+          d="M11.5 30c3.5-1 18.5-1 22 0M12 33.5c4-1.5 17-1.5 21 0"
+          fill="none"
+          stroke="#fff"
+        />
+        <circle cx="6" cy="12" r="2" fill="#fff" stroke="none" />
+        <circle cx="14" cy="9" r="2" fill="#fff" stroke="none" />
+        <circle cx="22.5" cy="8" r="2" fill="#fff" stroke="none" />
+        <circle cx="31" cy="9" r="2" fill="#fff" stroke="none" />
+        <circle cx="39" cy="12" r="2" fill="#fff" stroke="none" />
+      </g>
+    </svg>
+  ),
+  "♜": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fillRule="evenodd"
+        fill="#000"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path
+          d="M9 39h27v-3H9v3zM12.5 32l1.5-2.5h17l1.5 2.5h-20zM12 36v-4h21v4H12z"
+          strokeLinecap="butt"
+        />
+        <path
+          d="M14 29.5v-13h17v13H14z"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+        />
+        <path
+          d="M14 16.5L11 14h23l-3 2.5H14zM11 14V9h4v2h5V9h5v2h5V9h4v5H11z"
+          strokeLinecap="butt"
+        />
+        <path
+          d="M12 35.5h21M13 31.5h19M14 29.5h17M14 16.5h17M11 14h23"
+          fill="none"
+          stroke="#fff"
+          strokeWidth="1"
+          strokeLinejoin="miter"
+        />
+      </g>
+    </svg>
+  ),
+  "♝": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <g fill="#000" strokeLinecap="butt">
+          <path d="M9 36c3.39-.97 10.11.43 13.5-2 3.39 2.43 10.11 1.03 13.5 2 0 0 1.65.54 3 2-.68.97-1.65.99-3 .5-3.39-.97-10.11.46-13.5-1-3.39 1.46-10.11.03-13.5 1-1.354.49-2.323.47-3-.5 1.354-1.94 3-2 3-2z" />
+          <path d="M15 32c2.5 2.5 12.5 2.5 15 0 .5-1.5 0-2 0-2 0-2.5-2.5-4-2.5-4 5.5-1.5 6-11.5-5-15.5-11 4-10.5 14-5 15.5 0 0-2.5 1.5-2.5 4 0 0-.5.5 0 2z" />
+          <path d="M25 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+        </g>
+        <path
+          d="M17.5 26h10M15 30h15m-7.5-14.5v5M20 18h5"
+          stroke="#fff"
+          strokeLinejoin="miter"
+        />
+      </g>
+    </svg>
+  ),
+  "♞": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <g
+        opacity="1"
+        fill="none"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="#000000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit="4"
+        strokeDasharray="none"
+        strokeOpacity="1"
+        transform="translate(0,0.3)"
+      >
+        <path
+          d="M22 10C32.5 11 38.5 18 38 39H15C15 30 25 32.5 23 18"
+          fill="#000000"
+          stroke="#000000"
+        />
+
+        <path
+          d="M24 18C24.38 20.91 18.45 25.37 16 27C13 29 13.18 31.34 11 31C9.958 30.06 12.41 27.96 11 28C10 28 11.19 29.23 10 30C9 30 5.997 31 6 26C6 24 12 14 12 14C12 14 13.89 12.1 14 10.5C13.27 9.506 13.5 8.5 13.5 7.5C14.5 6.5 16.5 10 16.5 10H18.5C18.5 10 19.28 8.008 21 7C22 7 22 10 22 10"
+          fill="#000000"
+          stroke="#000000"
+        />
+
+        <path
+          d="M9.5 25.5A0.5 0.5 0 1 1 8.5 25.5A0.5 0.5 0 1 1 9.5 25.5z"
+          fill="#ffffff"
+          stroke="#ffffff"
+        />
+
+        <path
+          d="M15 15.5A0.5 1.5 0 1 1 14 15.5A0.5 1.5 0 1 1 15 15.5z"
+          transform="matrix(0.866,0.5,-0.5,0.866,9.693,-5.173)"
+          fill="#ffffff"
+          stroke="#ffffff"
+        />
+
+        <path
+          d="M24.55 10.4L24.1 11.85L24.6 12C27.75 13 30.25 14.49 32.5 18.75C34.75 23.01 35.75 29.06 35.25 39L35.2 39.5H37.45L37.5 39C38 28.94 36.62 22.15 34.25 17.66C31.88 13.17 28.46 11.02 25.06 10.5L24.55 10.4z"
+          fill="#ffffff"
+          stroke="none"
+        />
+      </g>
+    </svg>
+  ),
+  "♟": ({ size = 48 }) => (
+    <svg viewBox="0 0 45 45" width={size} height={size}>
+      <path
+        d="M22.5 9c-2.21 0-4 1.79-4 4 0 .89.29 1.71.78 2.38-1.95 1.12-3.28 3.21-3.28 5.62 0 2.03.94 3.84 2.41 5.03-3 1.06-7.41 5.55-7.41 13.47h23c0-7.92-4.41-12.41-7.41-13.47 1.47-1.19 2.41-3 2.41-5.03 0-2.41-1.33-4.5-3.28-5.62.49-.67.78-1.49.78-2.38 0-2.21-1.79-4-4-4z"
+        fill="#000"
+        stroke="#000"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  ),
+};
 /**
  * @param {string|null} value      - Ký tự quân cờ hoặc null/""
+ * @param {string|null} value      - Ký tự quân cờ unicode (♔♕♖♗♘♙♚♛♜♝♞♟) hoặc null/""
  * @param {number}      row        - Hàng (0–7)
  * @param {number}      col        - Cột (0–7)
  * @param {boolean}     isSelected - Ô đang được chọn
  * @param {function}    onClick    - Callback khi click
  */
-const Cell =({value,row,col,isSelected,isLegalTarget,onClick})=>{
-
-    const isLight = (row+col)%2 === 0;
-     const bgColor = isSelected
-    ? "#f6f669"                              // Ô đang chọn → vàng
+const Cell = ({ value, row, col, isSelected, isLegalTarget, onClick }) => {
+  const isLight = (row + col) % 2 === 0;
+  const bgColor = isSelected
+    ? "#f6f669" // Ô đang chọn → vàng
     : isLight
-    ? "#f0d9b5"                              // Ô sáng
-    : "#b58863";                             // Ô tối
-    return (
+    ? "#f0d9b5" // Ô sáng
+    : "#b58863"; // Ô tối
+  const unicode = value ? PIECE_MAP[value] : null;
+  const PieceIcon = unicode ? PIECE[unicode] : null;
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        width: `${CELL_SIZE}px`,
+        height: `${CELL_SIZE}px`,
+        border: isSelected ? "2px solid #baca2b" : "1px solid #999",
+        boxSizing: "border-box",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: bgColor,
+        cursor: value || isLegalTarget ? "pointer" : "default",
+        userSelect: "none",
+        transition: "background-color 0.15s",
+        position: "relative",
+      }}
+    >
+      {/* Highlight ô hợp lệ */}
+      {isLegalTarget && !value && (
+        // Ô trống → chấm tròn ở giữa
         <div
-            onClick={onClick}
-            style={{
-                width: `${CELL_SIZE}px`,
-                height: `${CELL_SIZE}px`,
-                border: isSelected ? "2px solid #baca2b" : "1px solid #999",
-                boxSizing: "border-box",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                fontSize: "28px",
-                backgroundColor: bgColor,
-                cursor: value ? "pointer" : "default",
-                userSelect: "none",
-                transition: "background-color 0.15s",
-                 position: "relative",
-        }}
-        >
-            {/* Highlight ô hợp lệ */}
-            {isLegalTarget &&(
-                 // Ô trống → chấm tròn ở giữa
-                <div style ={{
-                    position: "absolute",
-                    width: "24px",
-                    height: "24px",
-                    borderRadius: "50%",
-                    backgroundColor: "rgba(0,0,0,0.20)",
-                    pointerEvents: "none",
-                }}
-                />
-            )}
-            {isLegalTarget && value && (
-                // Có quân địch → viền vòng tròn (có thể ăn)
-                <div style={{
-                position: "absolute",
-                inset: 0,
-                borderRadius: "50%",
-                border: "5px solid rgba(0,0,0,0.22)",
-                pointerEvents: "none",
-        }} />
+          style={{
+            position: "absolute",
+            width: "28px",
+            height: "28px",
+            borderRadius: "50%",
+            backgroundColor: "rgba(0,0,0,0.18)",
+            pointerEvents: "none",
+            zIndex: 1,
+          }}
+        />
       )}
-
-            {value}
+      {isLegalTarget && value && (
+        // Có quân địch → viền vòng tròn (có thể ăn)
+        <div
+          style={{
+            position: "absolute",
+            inset: 0,
+            borderRadius: "50%",
+            border: "5px solid rgba(0,0,0,0.22)",
+            pointerEvents: "none",
+            zIndex: 2,
+          }}
+        />
+      )}
+      {/* Render SVG piece icon */}
+      {PieceIcon && (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            filter: "drop-shadow(0 1px 2px rgba(0,0,0,0.35))",
+            zIndex: 3,
+          }}
+        >
+          <PieceIcon size={44} />
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 export default Cell;
